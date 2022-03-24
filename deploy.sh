b84c58777441f865ac3f4a94286d7462c07b91f5
huyviet2582000 <huyviet2582000@gmail.com>
2022-03-24 12:10:52 +0700

    [FIX] deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ python manage.py migrate
 
 # run tests
 echo "Run server..."
-sudo gunicorn core.wsgi:application \
+gunicorn core.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3 \
     --daemon
