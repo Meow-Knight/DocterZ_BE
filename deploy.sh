a555f4387bbee7d38e240389f51522eb393c6a75
huyviet2582000 <huyviet2582000@gmail.com>
2022-03-24 12:09:38 +0700

    [FIX] deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ python3 -m venv env
 
 # source python env
 echo ">>>>>>Sourcing python env..."
-source env/bin/activate
+. env/bin/activate
 
 # install requirements
 pip install -r requirements.txt
