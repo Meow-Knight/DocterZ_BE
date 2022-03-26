c8a13c7f0e9c66f882ee07211a2bfbd529769121
nguyencongvinh <nguyencongvinh1412@gmail.com>
2022-03-26 21:42:36 +0700

    [FIX] update doctor profile API

--- api_doctor/views/Doctor.py
+++ api_doctor/views/Doctor.py
@@ -15,12 +15,23 @@ class DoctorViewSet(BaseViewSet):
     permission_map = {
         "signup": [AdminPermission],
         "login": [],
-        "update": [DoctorOrAdminPermission]
+        "edit_own_profile": [DoctorPermission],
     }
 
     @action(detail=False, methods=['post'], url_path='signup')
     def signup(self, request, *args, **kwargs):
         response_data = DoctorService.signup(request)
-        res_data = {"message": "Đăng ký thành công", "profile": response_data}
+        res_data = {"message": "Đăng ký thành công", "data": response_data}
         return Response(res_data, status=status.HTTP_201_CREATED)
 
+    @action(methods=['put'], detail=False)
+    def edit_own_profile(self, request, *args, **kwargs):
+        data = request.data
+        acc = request.user
+        doctor = Doctor.objects.filter(account=acc)
+        if doctor.exists():
+            doctor = doctor.first()
+            serializer = self.get_serializer(doctor, data=data, partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
