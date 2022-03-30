95cb2fae0a091a3f400dd16a1c83d24fff9fad65
huyviet2582000 <huyviet2582000@gmail.com>
2022-03-30 23:12:35 +0700

    [FIX] check CLOUDINARY_AVATAR_USER_FOLDER attribute .env

--- api_base/services/Cloudinary.py
+++ api_base/services/Cloudinary.py
@@ -3,6 +3,7 @@ import cloudinary.api
 import cloudinary.uploader
 
 from dotenv import load_dotenv
+from rest_framework.exceptions import ValidationError
 
 load_dotenv()
 
@@ -15,6 +16,8 @@ class CloudinaryService:
     @classmethod
     def delete_image(cls, link, folder_name):
         if link:
+            if not folder_name:
+                raise ValidationError({"error": "ENV not loaded"})
             pub_id = folder_name + link.split("/")[-1].split(".")[0]
             res = cloudinary.uploader.destroy(pub_id, folder=folder_name)
             if res['result'] == "ok":
